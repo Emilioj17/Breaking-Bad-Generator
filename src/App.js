14fcce1fbe7921042aae0c34093669f352b78f49
Emilioj17 <ejjelves@gmail.com>
2021-08-05 10:47:58 -0400

    Ok, funcionando

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import './App.css';
 import Cita1 from "./components/Cita1";
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+
+const Boton = styled.button`
+  background-color: green;
+  padding: 10px;
+  text-align: center;
+  width: 50%;
+  margin: 0 auto;
+`
 
 function App() {
   const [cita1, setCita1] = useState([]);
 
+  useEffect(() => { handlerOnClick1() }, []);
+
   const handlerOnClick1 = () => {
-        
+      fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes").then((response) => response.json())
+        .then((data) => {
+          setCita1(data);
+        })
+        .catch(err => (console.error(err)));
   }
 
-  const handlerOnClick2 = () => {
+  const handlerOnClick2 = async () => {
+    const response = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const data = await response.json();
+    setCita1(data);
         
   }
   return (
     <Fragment>
       <Cita1 cita1={cita1}/>
-      <button onClick={(e) => handlerOnClick1(e)}>Nueva Cita1</button>
-      <button onClick={(e)=>handlerOnClick2(e)}>Nueva Cita2</button>
+      <Boton onClick={(e) => handlerOnClick1(e)}>Fetch</Boton>
+      <Boton onClick={(e)=>handlerOnClick2(e)}>Async await</Boton>
     </Fragment>
   );
 }
--- src/components/Cita1.js
+++ src/components/Cita1.js
@@ -4,11 +4,19 @@ import styled from '@emotion/styled';
 const DivCita = styled.div`
   text-align: center;
   padding: 20px;
-  margin-top:60px;
+  margin-top:170px;
 
   div{
     background-color: white;
     margin: 20px;
+  }
+
+  p, h3 {
+      padding:20px;
+  }
+
+  p{
+      text-align: end;
   }
 `
 
@@ -16,8 +24,8 @@ function Cita1({ cita1 }) {
     return (
       <DivCita>
           <div>
-                <h3>Cita1: {cita1.cita}</h3>
-                <p>Autor: {cita1.autor}</p>
+                {cita1[0] ? <h3>{cita1[0].quote}</h3> : <h3>Cargando Cita...</h3>}
+                {cita1[0] ? <p>{cita1[0].author}</p>: <p>Cargando Autor...</p>}
           </div>
       </DivCita>
     );
